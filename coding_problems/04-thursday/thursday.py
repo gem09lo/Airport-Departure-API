852c4bb7f9a3adc572694e5285e7d354aacc4143
gem09lo <gemmaw09lo@gmail.com>
2025-07-14 17:06:28 +0100

    cleaned up files and repo root

--- coding_problems/04-thursday/thursday.py
+++
@@ -1,9 +0,0 @@
-def second_symbol(s: str, symbol: str) -> int:
-    occurrence = 0
-
-    for i, char in enumerate(s):
-        if char == symbol:
-            occurrence += 1
-            if occurrence == 2:
-                return i
-    return -1
